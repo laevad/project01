70242e7282825ee420e16aca041bdb6515c78dd8
laevad <aldave.tabian@ustp.edu.ph>
2022-10-31 17:47:24 +0800

    metas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <title>Cover Template · Bootstrap v5.0</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <title>test</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <style>
         .bd-placeholder-img {
